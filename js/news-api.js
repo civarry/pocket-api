2516651c89c6c904ba178f5e8090dc1f3a1fcfc7
civarry <cjcarito@gmail.com>
2023-05-02 15:24:37 +0800

    fixing the news

--- js/news-api.js
+++ js/news-api.js
@@ -1,17 +1,19 @@
-export async function getNewsData(query) {
+export function getNewsData(query) {
   const apiKey = "fd1e26876ed1484eb4f5d595ff52a19a";
   const url = `https://newsapi.org/v2/everything?q=${query}&from=2023-04-02&sortBy=publishedAt&apiKey=${apiKey}`;
   console.log(url);
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("There was a problem fetching news data:", error);
-    throw error;
-  }
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("There was a problem fetching news data:", error);
+      throw error;
+    });
 }
